f4c7926c973306f8682b1e854ab25b82e0129b1a
Andrey <bazhenova790@gmail.com>
2026-09-25 11:42:17 +0500

    Удалена дублирующая папка new-listing - фикс сборки на Vercel

--- src/app/new-listing/page.jsx
+++
@@ -1,9 +0,0 @@
-import NewListingForm from "@/components/NewListingForm";
-
-export default function NewListingPage() {
-  return (
-    <main className="min-h-screen p-4 sm:p-8 bg-slate-950 flex items-center justify-center">
-      <NewListingForm userCity="Актау" />
-    </main>
-  );
-}
